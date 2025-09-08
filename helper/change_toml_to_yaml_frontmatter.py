9c12c88a4e301e20d40bc7ac7b3d07454dcb207a
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-09-08 09:37:33 +0330

    Add change_toml_to_yaml_frontmatter

---
+++ helper/change_toml_to_yaml_frontmatter.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+
+def main():
+    p = Path("../site/content")
+    for path in p.glob("**/*.md"):
+        print(path)
+
+        with open(path) as f:
+            content = f.read()
+            lines = content.split("\n")
+            for i, line in enumerate(lines):
+                lines[i] = line.replace("+++", "---")
+                lines[i] = lines[i].replace(" = ", ": ")
+
+            result = "\n".join(lines)
+        with open(path, "w") as f:
+            f.write(result)
+
+
+if __name__ == '__main__':
+    main()
