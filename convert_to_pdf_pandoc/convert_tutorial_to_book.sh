bb9d740b238e0395315c3dd92ca78b366ea06bed
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-09-09 14:15:36 +0330

    Fix the destination path to be able to have the output in docs

--- convert_to_pdf_pandoc/convert_tutorial_to_book.sh
+++ convert_to_pdf_pandoc/convert_tutorial_to_book.sh
@@ -21,6 +21,7 @@ for tutorial in $dir_path/*; do
 
       # Define destination path
       destination_path=$(echo "$tutorial" | sed "s:/site/content/:/site/public/pdf/:g" )
+      destination_path=$destination_path/docs
       if [ ! -d "$destination_path" ]; then
         mkdir -p "$destination_path"
       fi
@@ -73,7 +74,7 @@ for tutorial in $dir_path/*; do
         --resource-path="$resources" \
         --pdf-engine=xelatex \
         --metadata author="Ramin Zarebidoky (LiterallyTheOne)" \
-        --metadata title="$(basename "$destination_path")" \
+        --metadata title="$(basename "$(dirname "$destination_path")" )" \
         --include-in-header="$header_path" \
         --highlight-style=tango \
         --lua-filter "$date_format_path"
@@ -84,51 +85,3 @@ for tutorial in $dir_path/*; do
 
     fi
 done
-#
-#
-#find "$dir_path" -name "index.md" -print0 | while IFS= read -r -d $'\0' file; do
-#
-#    should_delete=("pandoc.md")
-#
-#    parent_file=$(dirname "$file")
-#
-#    destination_path=$(echo "$parent_file" | sed "s:/site/content/:/site/public/pdf/:g" )
-#    if [ ! -d "$destination_path" ]; then
-#      mkdir -p "$destination_path"
-#    fi
-#
-#    echo "$parent_file"
-#    cd "$parent_file"
-#
-#    shopt -s nullglob
-#
-#    for img in *.webp; do
-#        magick "$img" "${img%.webp}.png"
-#        should_delete+=("${img%.webp}.png")
-#    done
-#
-#    for img in *.gif; do
-#        magick "${img}[0]" "${img%.gif}.png"
-#        should_delete+=("${img%.gif}.png")
-#    done
-#
-#    shopt -u nullglob
-#
-#    sed "s:\.webp:\.png:g" index.md > pandoc.md
-#    sed -i "" "s:\.gif:\.png:g" pandoc.md
-#
-#    pandoc \
-#        pandoc.md \
-#        -o "$destination_path"/output.pdf \
-#        --pdf-engine=xelatex \
-#        --metadata author="Ramin Zarebidoky (LiterallyTheOne)" \
-#        --include-in-header="$header_path" \
-#        --highlight-style=tango \
-#        --lua-filter "$date_format_path"
-#
-#    for x in "${should_delete[@]}"; do
-#      rm "$x"
-#    done
-#
-#
-#done
