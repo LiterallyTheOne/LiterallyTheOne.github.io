7a1aec02f38c68867943f4dff5e67d75142369ed
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-09-09 11:38:04 +0330

    Fix the date-format.lua to work with GitHub workflows.

--- convert_to_pdf_pandoc/date-format.lua
+++ convert_to_pdf_pandoc/date-format.lua
@@ -7,7 +7,7 @@
           local date = os.time({ year = y, month = m, day = d })
           -- Change the format string below to your desired output format
           local date_string = os.date("%d %b %Y", date) -- Example: 02 Apr 2018
-          meta.date = pandoc.Str(date_string)
+          meta.date = pandoc.MetaString(date_string)
         end
       end
       return meta
